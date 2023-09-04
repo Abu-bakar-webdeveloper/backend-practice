32b44a1a3d823ba0311d7d5d0fa1b889307374f8
abubkar <abubakaraw8@gmail.com>
2023-09-04 10:53:15 +0500

    loginuser and getme method added

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,24 @@
+// desc Register new user
+// route Post/api/users
+// access public
 const registerUser = (req, res) => {
     res.json({message: 'register user'})
+}
+// desc authenticate a user
+// route Post/api/users/login
+// access public
+const loginUser = (req, res) => {
+    res.json({message: 'login user'})
+}
+// desc Get user data
+// route Post/api/users/me
+// access public
+const getMe = (req, res) => {
+    res.json({message: 'User data'})
 }
 
 module.exports = {
     registerUser,
+    loginUser,
+    getMe
 }
